f5b37299f48d3798f5b1dbd387a8c3dc1b050286
Leo Lu <ileostarat1024@gmail.com>
2024-04-18 00:20:35 +0800

    First assignment + allsetting

---
+++ 240417_Assignment1.sql
@@ -0,0 +1,25 @@
+/*SELECT *
+FROM admin_regions
+LIMIT 5; 
+*/
+
+/*SELECT confName, divName, fullName
+FROM teams
+LIMIT 10;
+*/
+
+/*SELECT 	firstName AS first_name,
+		lastName AS last_name
+FROM players
+LIMIT 5;
+*/
+
+/*SELECT DISTINCT county
+FROM admin_regions;
+*/
+
+/*SELECT DISTINCT divName
+FROM teams;
+*/
+
+
